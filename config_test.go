b66234f2cd862616b620e3580a6b4acef6925b8c
Colton Wolkins (Ogre) <frostyfrog2@gmail.com>
2014-08-09 19:20:44 -0600

    Added custom config file test

--- config_test.go
+++ config_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"testing"
-	//	"os"
+	"os"
 	. "github.com/franela/goblin"
 )
 
@@ -20,7 +20,19 @@ func TestConfig(t *testing.T) {
 			config.Load()
 			g.Assert(1 + 1).Equal(2)
 		})
-		g.It("Should load custom config file")
+		g.It("Should load custom config file", func() {
+			fo, err := os.Create("output.txt")
+			if err != nil { g.Fail("Unable to open config file for writing") }
+			defer fo.Close()
+			fo.Write([]byte(`Projects
+			- mice
+			- golang
+			- testing
+			- gopher
+			- goblin`))
+			config := SystemConfig{}
+			config.ReadFile("output.txt")
+		})
 		g.It("config file should have array of projects")
 		g.It("should be able to load project config")
 	})
